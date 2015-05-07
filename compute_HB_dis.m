c9f625853fd8d2a1730007f8da48732bbcfb43e4
Tong Wang <wangtongada@126.com>
2015-05-07 11:44:50 -0400

    Update compute_HB_dis.m

--- compute_HB_dis.m
+++ compute_HB_dis.m
@@ -1,9 +1,10 @@
 % compute the similarity in geographic locations
-% data geocode contains the longitutde and latitude of each crime.
+% geo_coordinate contains the longitutde and latitude of each crime.
 
 HB_dis=zeros(Ncrimes,Ncrimes);
+d = 0.2
 for i=1:Ncrimes
     for j=(i+1):Ncrimes
-        HB_dis(i,j)=distdim(distance(geocode(i,1),geocode(i,2),geocode(j,1),geocode(j,2)),'deg','kilometers');
+        HB_dis(i,j)=exp(-distdim(distance('gc',geo_coordinate(i,:),geo_coordinate(j,:)),'deg','km')/d);
     end
 end
