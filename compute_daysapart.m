7fda3e1af9d4d7dd34473a1e015c3a0cc4f63d49
Tong Wang <wangtongada@126.com>
2015-05-07 11:48:25 -0400

    Update compute_daysapart.m

--- compute_daysapart.m
+++ compute_daysapart.m
@@ -5,7 +5,7 @@ lamda=90; % this number can be customized
 for i=1:Ncrimes
     di=datenum(date_data(i,:));
     for j=(i+1):Ncrimes
-        dj=datenum(date_data(j,4:6));
+        dj=datenum(date_data(j,:));
         dayapart(i,j)=exp(-abs(di-dj)/lamda);
     end
 end
